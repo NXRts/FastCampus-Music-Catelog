53c39b92e5b75ed2af4a381d7c8fd838de77193c
NXRts <Dan_gou_U_9252>
2024-12-17 15:51:35 +0700

    Initial setup for music catalog project

---
+++ internal/configs/types.go
@@ -0,0 +1,17 @@
+package configs
+
+type (
+	Config struct {
+		Service  Service  `mapstructure:"Service"`
+		Database Database `mapstructure:"Database"`
+	}
+
+	Service struct {
+		Port      string `mapstructure:"port"`
+		SecretJWT string `mapstructure:"secreateJWT"`
+	}
+
+	Database struct {
+		DataSourceName string `mapstructure:"dataSourceName"`
+	}
+)
